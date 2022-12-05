13798e6c5dab55b91c202984c2c64e6bacce60f7
Tsebo Moisi <42075412+TseboMoisi@users.noreply.github.com>
2022-12-05 17:38:37 +0200

    Add files via upload

---
+++ MailBag/Client/src/code/IMAP.ts
@@ -0,0 +1,45 @@
+import axios, { AxiosResponse } from "axios";
+
+import { config } from "./config";
+
+
+export interface IMailbox { name: string, path: string }
+
+
+export interface IMessage {
+    id: string,
+    date: string,
+    from: string,
+    subject: string,
+    body?: string
+}
+
+
+export class Worker {
+
+
+    public async listMailboxes(): Promise<IMailbox[]> {
+
+        console.log("IMAP.Worker.listMailboxes()");
+
+        const response: AxiosResponse = await axios.get(config.serverAddress + '/mailboxes');
+        return response.data;
+
+    }
+
+
+    public async getMessageBody(inID: string, inMailbox: String): Promise<string> {
+        
+        console.log("IMAP.Worker.getMessageBody()", inID);
+
+        const response: AxiosResponse = await axios.get(config.serverAddress + '/messages/' + inMailbox + '/' + inID);
+        return response.data;
+    }
+
+    public async deleteMessage(inID: string, inMailbox: String): Promise<void> {
+
+        console.log("IMAP.Worker.getMessageBody()", inID);
+
+        await axios.delete(config.serverAddress + '/messages/' + inMailbox + '/' + inID);
+    }
+}
